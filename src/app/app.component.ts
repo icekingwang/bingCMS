20f9f87f63d4e743d6689c421d08d0e627aa39ca
181204kk <jiangsu.pxkk2008@163.com>
2017-04-05 19:40:08 +0800

    登陆的service ，而且在app 增加了回调（还不理解）。

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {UserLoginService} from "./user/user-login/user-login.service";
+import {User} from "./user/model/user-model";
+import {RouterState, RouterStateSnapshot, Router} from "@angular/router";
 
 @Component({
   selector: 'app-root',
@@ -7,14 +10,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
-  active = 'wenzhang';
-  routerLinkActive = 'wenzhang';
+  count = 0;
+  	public currentUser : User;
 
-  checkTab(t){
-    return t===this.active;
+
+  constructor(public userLoginService:UserLoginService,
+              public router: Router,
+  ){
   }
+  ngOnInit(){
+    this.currentUser = JSON.parse(localStorage.getItem("currentUser"));
 
-  clickTabFun(t){
-    this.active=t;
+
+    console.log(this.userLoginService.currentUser);
+    this.userLoginService.currentUser.subscribe(
+      next=>{
+        this.currentUser = next;
+        this.count++;
+        console.log("运行第"+ this.count + "次+++++++++++++++++++++++/n");
+
+        let routerState: RouterState = this.router.routerState;
+        let routerStateSnapshot: RouterStateSnapshot = routerState.snapshot;
+        if(routerStateSnapshot.url.indexOf("/login")!=-1){
+          this.router.navigateByUrl("/home");
+        }
+      },
+      error=>{}
+
+    )
   }
 }
--- src/app/user/user-login/user-login.service.ts
+++ src/app/user/user-login/user-login.service.ts
@@ -1,20 +1,42 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {User} from "../model/user-model";
-import {Http} from "@angular/http";
+import {Http, Response} from "@angular/http";
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import {Subject} from 'rxjs/Subject';
 
 @Injectable()
 export class UserLoginService {
 
   public userLoginUrl = "http://localhost:3003/users";
+  public subject: Subject<User> = new Subject<User>();
 
-  constructor(public http:Http) {
+  constructor(public http: Http) {}
 
+  public get currentUser():Subject<User>{
+    return this.subject;
   }
 
-  public login(user:User){
-      console.log(this.http)
-      console.log(this)
-    this.http.get(this.userLoginUrl).subscribe(data=>console.log(data));
+  public login(user: User) {
+    console.log(user);
+    let userLoginPUrl = this.userLoginUrl + "?userName=" + user.userName + "#password=" + user.password;
+    this.http
+      .get(userLoginPUrl).map(
+      (response: Response) => {
+        console.log(response);
+        let user = response.json();
+        console.log(user);
+        console.log(this.subject);
+        if(user){
+          localStorage.setItem("currentUser",JSON.stringify(user));
+          this.subject.next(Object.assign({},user));
+        }
+
+        return response;
+      })
+      .subscribe(data=>console.log(data),
+        error => console.log(error)
+      );
   }
 
 }
